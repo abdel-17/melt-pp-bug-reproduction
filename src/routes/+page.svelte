d9e6041fa2d8f9172f99e55c6006136ca7f39416
abdel-17 <abdelrahmanb015@gmail.com>
2023-12-19 03:33:07 +0200

    install melt-ui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { createToggle, melt } from "@melt-ui/svelte";
+
+	let pressed = $state(false);
+	let disabled = $state(false);
+
+	const {
+		elements: { root },
+	} = createToggle({
+		defaultPressed: pressed,
+		disabled,
+	});
+</script>
+
+<button use:melt={$root}>Toggle</button>
--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,15 @@
-import adapter from '@sveltejs/adapter-auto';
+import { preprocessMeltUI } from "@melt-ui/pp";
+import adapter from "@sveltejs/adapter-auto";
+import sequence from "svelte-sequential-preprocessor";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: sequence([preprocessMeltUI()]),
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
+		adapter: adapter(),
+	},
 };
-
 export default config;
